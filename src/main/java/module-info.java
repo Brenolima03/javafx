ba4dc080232bd0ca772be13cd48fa1c4d0ec31f1
brenolima03 <brenocslima@outlook.com>
2023-07-18 11:30:39 -0400

    DepartmentService

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module application {
 
     opens application to javafx.fxml;
     opens gui to javafx.fxml;
-    opens model.entities to javafx.fxml;
+    opens model.entities to javafx.base;
+
+
     exports application;
 }

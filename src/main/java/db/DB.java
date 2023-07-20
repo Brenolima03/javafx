63a5d7c201d4d6d781067683c870d8b010c19bd9
brenolima03 <brenocslima@outlook.com>
2023-07-20 11:30:33 -0400

    Remove department

--- src/main/java/db/DB.java
+++ src/main/java/db/DB.java
@@ -1,12 +1,15 @@
 package db;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -16,9 +19,11 @@ public class DB {
     public static Connection getConnection() {
         if (conn == null) {
             try {
-                // Utilizando as propriedades do H2 do arquivo db.properties
+                // Using the H2 properties from the db.properties file
                 conn = DriverManager.getConnection(getProperties("dburl"), getProperties("user"),
                         getProperties("password"));
+                
+                createDatabaseIfNotExists();
             } catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
@@ -26,6 +31,29 @@ public class DB {
         return conn;
     }
 
+    public static void createDatabaseIfNotExists() {
+        try (Connection conn = DriverManager.getConnection(getProperties("dburl"), getProperties("user"),
+                getProperties("password"));
+             Statement st = conn.createStatement()) {
+
+            // Check if the "Department" table exists
+            ResultSet rs = conn.getMetaData().getTables(null, null, "Department", null);
+            if (!rs.next()) {
+                // The "Department" table does not exist, so we'll create it
+                
+                // Read the content from the schema.sql file
+                String sqlScript = readSqlScriptFromFile("schema.sql");
+
+                // Execute the SQL script to create the "Department" table
+                st.executeUpdate(sqlScript);
+            }
+
+        } catch (SQLException | IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    // ... (remaining code)
     public static void closeConnection() {
         if (conn != null) {
             try {
@@ -46,9 +74,36 @@ public class DB {
         }
     }
 
+    private static String readSqlScriptFromFile(String fileName) throws IOException {
+        StringBuilder content = new StringBuilder();
+        try (InputStream input = DB.class.getClassLoader().getResourceAsStream(fileName);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+        }
+        return content.toString();
+    }
+
     public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
     }
 
     public static void closeStatement(PreparedStatement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
     }
 }
